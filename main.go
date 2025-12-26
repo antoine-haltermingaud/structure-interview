8c7cca64360eb7a0f792599d554d2248d810227f
Antoine Halter-Mingaud <antoine.haltermingaud@gmail.com>
2025-12-26 10:54:36 +0100

    adding tree data structure

--- main.go
+++ main.go
@@ -8,6 +8,42 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+type PriceNode struct {
+	Price 		float64
+	Quantity 	float64
+	Left 			*PriceNode
+	Right			*PriceNode
+}
+
+func (n *PriceNode) Insert(quantity, price float64, isBid bool) *PriceNode{
+	if n == nil { //pointer doesn't exist -> returns new one
+		return &PriceNode{Quantity: quantity, Price: price}
+	}
+	if price == n.Price { 
+		n.Quantity = quantity
+		return n
+	}
+	if price > n.Price {
+		n.Right = n.Right.Insert(quantity, price, isBid)
+	} else {
+		n.Left = n.Left.Insert(quantity, price, isBid)
+	}
+	return n 
+
+}
+
+func GetDescendingTop10(n *PriceNode, results *[]PriceNode) {
+	if n == nil || len(*results) >= 10 {
+		return
+	}
+	GetDescendingTop10(n.Right, results)
+	if len(*results) < 10 && n.Quantity > 0 {
+		*results = append(*results, *n)
+	}
+	GetDescendingTop10(n.Left, results)
+}
+
+
 func main() {
 	url := "wss://stream.binance.com:9443/ws/btcusdc@depth"
 
@@ -19,6 +55,7 @@ func main() {
 	defer c.Close()
 
 	fmt.Println("Reading stream")
+	bidsBst := &PriceNode{}
 
 	for {
 		_, message, _ := c.ReadMessage()
@@ -29,9 +66,12 @@ func main() {
 		asks, _ := raw["a"].([]any)
 
 		if len(bids) > 0 && len(asks) > 0 {
-			topBid := bids[0].([]any)
-			topAsk := asks[0].([]any)
-			fmt.Printf("price: %s | qty: %s  bid ||| price: %s | qty: %s  ask\n", topBid[0], topBid[1], topAsk[0], topAsk[1])
+			topBid := bids[0].([]float64)
+			//topAsk := asks[0].([]any)
+			// fmt.Printf("price: %s | qty: %s  bid ||| price: %s | qty: %s  ask\n", topBid[0], topBid[1], topAsk[0], topAsk[1])
+			quantity := topBid[1]
+			bidPrice := topBid[0]
+			bidsBst.Insert(quantity, bidPrice, true)
 		}
 	}
 }
